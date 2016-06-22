8d815fd062eff7b17aa555b5f18b258ae6349ed6
Alper Kucukural <alper.kucukural@umassmed.edu>
2016-06-22 18:41:23 +0000

    s3hashcalc added

---
+++ s3getList.py
@@ -0,0 +1,68 @@
+#!/share/bin/python
+
+from optparse import OptionParser
+import os
+import urllib, json
+import re
+import sys
+import re
+import cgi
+
+
+url="http://dolphin.umassmed.edu/ajax/dolphinfuncs.php?p=getFileList"
+   
+def getFileList():
+
+    response = urllib.urlopen(url);
+    data = json.loads(response.read())
+
+    return data
+
+def getValfromFile(filename):
+    val=''
+    if (os.path.isfile(filename)):
+    	infile = open(filename)
+    	val = infile.readlines()[0].rstrip()
+    else:
+    	sys.exit(84)
+    return val
+ 
+def getList(amazonbucket, inputfile):
+   amazonbucket = re.sub('s3://biocorebackup/', '', amazonbucket)
+   command="s3cmd ls s3://biocorebackup/"+amazonbucket+"/"+inputfile
+   print command
+   child = os.popen(command)
+   jobout = child.read().rstrip()
+   return jobout 
+
+
+def runGetList(input, dirname, amazon_bucket):
+   files=input.split(',')
+   count=0
+   if (len(files)>1):
+     file1=getList(amazon_bucket, files[0].strip())
+     file2=getList(amazon_bucket, files[1].strip())
+     file="["+file1+"],["+file2+"]"
+   else:
+     file=getList(amazon_bucket, input.strip())
+
+   if (len(file) > 10 ):
+      print "\n\nExist:\n"+file+"\n\n"
+   else:
+      print "\n\nDon't Exist\n\n"
+
+def main():
+
+   results=getFileList()
+   for result in results:
+       
+       fastq_dir=result['fastq_dir']
+       filename=result['file_name']
+       amazon_bucket=result['amazon_bucket']
+       print fastq_dir 
+       print filename 
+       print amazon_bucket
+       runGetList(filename, fastq_dir, amazon_bucket)
+
+if __name__ == "__main__":
+    main()
---
+++ s3hashCalc.py
@@ -0,0 +1,99 @@
+#!/share/bin/python
+
+from optparse import OptionParser
+import os
+import urllib, json
+import re
+import sys
+import re
+import cgi
+
+
+url="http://dolphin.umassmed.edu/ajax/dolphinfuncs.php?p=getFileList"
+updateurl="http://dolphin.umassmed.edu/ajax/dolphinfuncs.php?p=updateHashBackup&input=%(input)s&dirname=%(dirname)s&hashstr=%(hashstr)s"
+   
+def getFileList():
+
+    response = urllib.urlopen(url);
+    data = json.loads(response.read())
+
+    return data
+
+def getValfromFile(filename):
+    val=''
+    if (os.path.isfile(filename)):
+    	infile = open(filename)
+    	val = infile.readlines()[0].rstrip()
+    else:
+    	sys.exit(84)
+    return val
+ 
+def calcHash(amazonbucket, inputfile):
+   command="mkdir -p /mnt/tmp; cd /mnt/tmp;"
+   amazonbucket = re.sub('s3://biocorebackup/', '', amazonbucket)
+   command+="s3cmd get --skip-existing s3://biocorebackup/"+amazonbucket+"/"+inputfile+">/dev/null;"
+   command+="md5sum "+inputfile+" > "+inputfile+".md5sum;"
+   print command
+   child = os.popen(command)
+   jobout = child.read().rstrip()
+   err = child.close()
+   hashstr = getValfromFile("/mnt/tmp/"+inputfile+".md5sum").split(" ")[0]
+   command="rm -rf /mnt/tmp/"+inputfile+"*"
+   child = os.popen(command)
+   if (len(hashstr) < 10):
+       return "" 
+   print hashstr
+   return hashstr
+
+def getLS(amazonbucket, inputfile):
+   amazonbucket = re.sub('s3://biocorebackup/', '', amazonbucket)
+   command="s3cmd ls s3://biocorebackup/"+amazonbucket+"/"+inputfile
+   print command
+   child = os.popen(command)
+   jobout = child.read().rstrip()
+   res = False
+   if (len(jobout) > 10 ):
+      print "\n\nExist:\n"+jobout+"\n\n"
+      res = True
+   else:
+      print "\n\nDon't Exist\n\n"
+   return res
+
+def runCalcHash(input, dirname, amazon_bucket):
+   files=input.split(',')
+   count=0
+   hash_str = ""
+   if (len(files)>1):
+       hashstr1 = ""
+       hashstr2 = ""
+       if (getLS(amazon_bucket, files[0].strip()) and getLS(amazon_bucket, files[1].strip())):
+          hashstr1=calcHash(amazon_bucket, files[0].strip())
+          hashstr2=calcHash(amazon_bucket, files[1].strip())
+       hashstr=hashstr1+","+hashstr2
+   else:
+       if (getLS(amazon_bucket, input.strip())):
+           hashstr=calcHash(amazon_bucket, input.strip())
+
+   if (len(hashstr)>10):
+     input=urllib.quote(input)
+     dirname=urllib.quote(dirname)
+     urlstr=updateurl%locals()
+     print urlstr
+     response = urllib.urlopen(urlstr)
+  
+
+def main():
+
+   results=getFileList()
+   for result in results:
+       
+       fastq_dir=result['fastq_dir']
+       filename=result['file_name']
+       amazon_bucket=result['amazon_bucket']
+       print fastq_dir 
+       print filename 
+       print amazon_bucket
+       runCalcHash(filename, fastq_dir, amazon_bucket)
+
+if __name__ == "__main__":
+    main()
