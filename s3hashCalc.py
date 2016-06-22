2bf2916e9e46ae4c8ed2082637f4499b9725bc4f
Alper Kucukural <alper.kucukural@umassmed.edu>
2016-06-22 19:20:17 +0000

    hash_str variable fix

--- s3hashCalc.py
+++ s3hashCalc.py
@@ -11,6 +11,9 @@ import cgi
 
 url="http://dolphin.umassmed.edu/ajax/dolphinfuncs.php?p=getFileList"
 updateurl="http://dolphin.umassmed.edu/ajax/dolphinfuncs.php?p=updateHashBackup&input=%(input)s&dirname=%(dirname)s&hashstr=%(hashstr)s"
+def stop_err( msg ):
+    sys.stderr.write( "%s\n" % msg )
+    sys.exit(2)
    
 def getFileList():
 
@@ -56,44 +59,48 @@ def getLS(amazonbucket, inputfile):
       print "\n\nExist:\n"+jobout+"\n\n"
       res = True
    else:
-      print "\n\nDon't Exist\n\n"
+      print "\n\nDoesn't Exist\n\n"
    return res
 
 def runCalcHash(input, dirname, amazon_bucket):
-   files=input.split(',')
-   count=0
-   hash_str = ""
-   if (len(files)>1):
-       hashstr1 = ""
-       hashstr2 = ""
-       if (getLS(amazon_bucket, files[0].strip()) and getLS(amazon_bucket, files[1].strip())):
-          hashstr1=calcHash(amazon_bucket, files[0].strip())
-          hashstr2=calcHash(amazon_bucket, files[1].strip())
-       hashstr=hashstr1+","+hashstr2
-   else:
-       if (getLS(amazon_bucket, input.strip())):
-           hashstr=calcHash(amazon_bucket, input.strip())
-
-   if (len(hashstr)>10):
-     input=urllib.quote(input)
-     dirname=urllib.quote(dirname)
-     urlstr=updateurl%locals()
-     print urlstr
-     response = urllib.urlopen(urlstr)
+   try:
+       files=input.split(',')
+       count=0
+       hashstr = ""
+       if (len(files)>1):
+           hashstr1 = ""
+           hashstr2 = ""
+           if (getLS(amazon_bucket, files[0].strip()) and getLS(amazon_bucket, files[1].strip())):
+              hashstr1=calcHash(amazon_bucket, files[0].strip())
+              hashstr2=calcHash(amazon_bucket, files[1].strip())
+           hashstr=hashstr1+","+hashstr2
+       else:
+           if (getLS(amazon_bucket, input.strip())):
+               hashstr=calcHash(amazon_bucket, input.strip())
+       if (len(hashstr)>10):
+         input=urllib.quote(input)
+         dirname=urllib.quote(dirname)
+         urlstr=updateurl%locals()
+         print urlstr
+         response = urllib.urlopen(urlstr)
+   except Exception, ex:
+        stop_err('Error (line:%s)running runCalcHash\n%s'%(format(sys.exc_info()[-1].tb_lineno), str(ex)))
   
 
 def main():
-
-   results=getFileList()
-   for result in results:
+   try:
+       results=getFileList()
+       for result in results:
        
-       fastq_dir=result['fastq_dir']
-       filename=result['file_name']
-       amazon_bucket=result['amazon_bucket']
-       print fastq_dir 
-       print filename 
-       print amazon_bucket
-       runCalcHash(filename, fastq_dir, amazon_bucket)
+           fastq_dir=result['fastq_dir']
+           filename=result['file_name']
+           amazon_bucket=result['amazon_bucket']
+           print fastq_dir 
+           print filename 
+           print amazon_bucket
+           runCalcHash(filename, fastq_dir, amazon_bucket)
+   except Exception, ex:
+        stop_err('Error (line:%s)running main\n%s'%(format(sys.exc_info()[-1].tb_lineno), str(ex)))
 
 if __name__ == "__main__":
     main()
